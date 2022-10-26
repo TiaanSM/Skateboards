f251dd885cb57eea02f668e59efc227bf10526f7
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-10-26 19:49:54 +0200

    Removed clutter, fixed navbar

--- furniture-react.js/src/Navbar.js
+++ furniture-react.js/src/Navbar.js
@@ -16,7 +16,7 @@ function CustomLink({to, children, ...props}) {
     const resolvedPath = useResolvedPath(to)
     const isActive = useMatch({ path: resolvedPath.pathname, end: true })
     return (
-        <li className={path === to ? 'active' : ''}>
+        <li className={isActive ? 'active' : ''}>
             <Link to={to} {...props}>
                 {children}
             </Link>
