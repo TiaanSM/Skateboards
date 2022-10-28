914e3745a769f68a90d2c6fe9411871a71592395
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-10-28 18:13:05 +0200

    Clean up for projects

--- furniture-react.js/src/App.js
+++ furniture-react.js/src/App.js
@@ -1,18 +1,9 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={ <Home /> } />
-        </Routes>
-      </div>
-    </>
+    <div></div>
   );
 }
 
