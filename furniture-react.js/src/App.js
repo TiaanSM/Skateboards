6fda9676f1c38efb4f3cd0b0101c78194558237c
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-10-29 16:01:22 +0200

    Added basic navbar for site

--- furniture-react.js/src/App.js
+++ furniture-react.js/src/App.js
@@ -1,10 +1,23 @@
-
-
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
 function App() {
 
   return (
-    <div></div>
-  );
+    
+    <div className="App">
+      <Navbar />
+      <Routes>
+          <Route path="/" element={ <Home /> }></Route>
+          <Route path="/about" element={ <About /> }></Route>
+          <Route path="/contact" element={ <Contact /> }></Route>
+      </Routes>
+  
+    </div>
+      
+  )
 }
 
 export default App;
--- furniture-react.js/src/component/Navbar.js
+++ furniture-react.js/src/component/Navbar.js
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-      
+    <nav className="nav">
+      <h1>Navbar</h1>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
   )
 }
