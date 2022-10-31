95f1d40f2f1dceed4b052cb45e2ed4f424e4806f
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-10-31 18:57:47 +0200

    Set up functionality for scroll count
    Implemented function to count scrolled value amount / amount the user scrolled

--- furniture-react.js/src/component/Header.js
+++ furniture-react.js/src/component/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+
 
 const Header = () => {
 
   // onWheel image zooms out from full screen to fixed smaller image.
   // element move of screen onWheel
-
-  
 
   const scale = () => {
 
@@ -20,8 +20,34 @@ const Header = () => {
       // transform: scale(1);
     }
   }
+  
+  // Counts the scroll value
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-  // image z index above background texts but under navbar
+  const handleScroll = () => {
+    const position = window.scrollY;
+    console.log(position);
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+  }, [scrollPosition]);
+
+
+  // Connect the scroll value to the scale amount of the image.
+  // image starts out at full screen then shrinks to a fixed size.
+
+  // if the fixed size is met then the header will unlock from 100vh and continue to next section.
+
+  // animation for scaling image, scroll value is image scale value
+
+  // header texts float upwards out of screen.
+
+  // when scrolling up the event occurs in reverse.
 
   return (
     <header onWheel={scale}>
