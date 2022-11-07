cc78083aa4e99a8e3b08107e31c31b71f99f9f05
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-11-07 19:20:56 +0200

    Worked on footer styling.
    Styled links and added input fields required.

--- furniture-react.js/src/component/Footer.jsx
+++ furniture-react.js/src/component/Footer.jsx
@@ -13,20 +13,34 @@ const Footer = () => {
       <form>
         <label for="input-1">Input 1</label>
         <input type="text" id="input-1" />
+
+        <label for="input-2">Input 2</label>
+        <input type="text" id="input-2" />
+
+        <label for="input-3">Input 3</label>
+        <input type="text" id="input-3" />
+
+        <label for="input-4">Input 4</label>
+        <input type="text" id="input-4" />
+
+        <label for="input-5">Input 5</label>
+        <input type="email" name="" id="input-5" />
+
+        <input type="submit" value="" />
       </form>
 
-      <div>
-        <p> &copy; SKATEBOARDS / All Rights Reserved</p>
-        <a href="">Public offer</a>
+      <span className="footer-links">
+        <p className="footer-tag"> &copy; SKATEBOARDS / All Rights Reserved</p>
+        <a href="" className="footer-tag">Public offer</a>
 
-        <div>
-          <a href="">Facebook</a>
-          <a href="">Instagram</a>
-          <a href="">Telegram</a>
-        </div>
+        <span className="footer-social-links">
+          <a href="" className="social-link">Facebook</a>
+          <a href="" className="social-link">Instagram</a>
+          <a href="" className="social-link">Telegram</a>
+        </span>
 
-        <p>Made by Tiaan McLeod</p>
-      </div>
+        <a href="" className="footer-tag">Made by Tiaan McLeod</a>
+      </span>
     </footer>
   )
 }
