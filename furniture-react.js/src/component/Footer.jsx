c1ce8f608224997268d2bd2f372d451c68824edd
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-11-08 13:38:30 +0200

    Worked on the footer form.
    Styled the inputs, button and layout of the footer section.

--- furniture-react.js/src/component/Footer.jsx
+++ furniture-react.js/src/component/Footer.jsx
@@ -2,31 +2,39 @@ import '../component/Footer.css';
 
 const Footer = () => {
 
-  // 5 x inputs
-  // 1 submit
-  // alert if not filled
+  
 
   return (
     <footer>
       <h3 className="footer-title">HAVE A QUESTION? THEN THERE IS AN ANSWER!</h3>
 
       <form>
-        <label for="input-1">Input 1</label>
-        <input type="text" id="input-1" />
+        <span className="input-border">
+          <label for="input-1">Input 1</label>
+          <input type="text" id="input" />
+        </span>
+      <div className="input-details">
+        <span className="input-border">
+          <label for="input-2">Input 2</label>
+          <input type="text" id="input" />
+        </span>
 
-        <label for="input-2">Input 2</label>
-        <input type="text" id="input-2" />
+        <span className="input-border">
+          <label for="input-3">Input 3</label>
+          <input type="text" id="input" />
+        </span>
 
-        <label for="input-3">Input 3</label>
-        <input type="text" id="input-3" />
+        <span className="input-border">
+          <label for="input-4">Input 4</label>
+          <input type="text" id="input" />
+        </span>
+      </div>
+        <span className="input-border">
+          <label for="input-5">Input 5</label>
+          <input type="email" name="" id="input" />
+        </span>
 
-        <label for="input-4">Input 4</label>
-        <input type="text" id="input-4" />
-
-        <label for="input-5">Input 5</label>
-        <input type="email" name="" id="input-5" />
-
-        <input type="submit" value="" />
+        <input type="submit" value="" id="submit" />
       </form>
 
       <span className="footer-links">
