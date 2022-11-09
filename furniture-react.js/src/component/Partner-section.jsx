521df20895c6a9928449cedc5e0ff5e9e9044247
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-11-09 12:56:33 +0200

    Added partner section.
    Styled catalogue title, added partner section component, planning for header.

---
+++ furniture-react.js/src/component/Partner-section.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../component/Partner.css';
+
+const Partner = () => {
+  return (
+    <section>
+        <img src="/" alt="" />
+        <h5>WE LOVE PARTNERSHIP</h5>
+        <p>
+            Situation: your place, be it a city cafe, deli or bar, has got everything in the world. Almost.
+            It is quite possible that you lack mr.pops. And this can be fixed.
+        </p>
+    </section>
+  )
+}
+
+export default Partner;
