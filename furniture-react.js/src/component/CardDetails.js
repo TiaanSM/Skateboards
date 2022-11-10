91f6cba27250a557ead979cb856ffdc39baabf15
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-11-10 14:55:47 +0200

    Worked on catalogue cards.
    Cards are displayed using the map method, card details have been moved to seperate file.

---
+++ furniture-react.js/src/component/CardDetails.js
@@ -0,0 +1,45 @@
+const CardDetails = [
+    {
+        id: '1',
+        design: 'ArtsyBoards',
+        image: require('../Assets/img/skateboard-1.jpg'),
+        size: '8.0',
+        price: 'R 1499'
+    },
+    {
+        id: '2',
+        design: 'ArtsyBoards',
+        image: require('../Assets/img/skateboard-2.jpg'),
+        size: '8.125',
+        price: 'R 1299'
+    },
+    {
+        id: '3',
+        design: 'ArtsyBoards',
+        image: require('../Assets/img/skateboard-3.jpg'),
+        size: '8.25',
+        price: 'R 1600'
+    },
+    {
+        id: '4',
+        design: 'ArtsyBoards',
+        image: require('../Assets/img/skateboard-4.jpg'),
+        size: '8.0',
+        price: 'R 1199'
+    },
+    {
+        id: '5',
+        design: 'ArtsyBoards',
+        image: require('../Assets/img/skateboard-5.jpg'),
+        size: '7.5',
+        price: 'R 999'
+    },
+    {
+        id: '6',
+        design: 'ArtsyBoards',
+        image: require('../Assets/img/skateboard-6.jpg'),
+        size: '8.25',
+        price: 'R 1399'
+    }
+]
+export default CardDetails;
