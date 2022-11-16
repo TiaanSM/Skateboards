17c5986b20f8a6522a30c3c0d2ce4333c268c4ea
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-11-16 15:33:10 +0200

    Added parralax effect to partner section.
    Added parralax effect, added planning for catalog modal.

--- furniture-react.js/src/component/layout/Catalogue.jsx
+++ furniture-react.js/src/component/layout/Catalogue.jsx
@@ -3,6 +3,15 @@ import CardDetails from '../CardDetails';
 import '../styles/Catalogue.css';
 
 const Modal = ({isOpen, onClose, CardDetails}) => {
+
+  // add to cart functionality to be added.
+  // display add to cart item in navbar counter.
+  // add item img, desc, price, amount and total.
+
+  // clean up code refactor and split components.
+
+  // imrpove minor details. and add to page when finished.
+  // make sure everything responsive and all links, buttons are working.
     
   return isOpen && (
     <div className="backdrop">
@@ -11,7 +20,7 @@ const Modal = ({isOpen, onClose, CardDetails}) => {
                   <div className="item-details">
                     <img src={CardDetails.image} alt="" width="98px" height="98px" />
                     <h4 className="item-name">{CardDetails.design}</h4>
-                    <p className="item-size">{CardDetails.size}</p>
+                    <p className="item-size">Size: {CardDetails.size}</p>
                     <h6 className="item-price">{CardDetails.price}</h6>
                     <button className="exit-modal" onClick={onClose}>
                       <svg width="22" height="22" viewBox="0 0 22 22" fill="red" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.2538 10.9594L0 21.2132L0.707107 21.9203L10.9609 11.6665L21.2148 21.9203L21.9219 21.2132L11.668 10.9594L21.9203 0.707107L21.2132 0L10.9609 10.2523L0.708672 0L0.00156516 0.707107L10.2538 10.9594Z"></path>
--- furniture-react.js/src/component/layout/Partner.jsx
+++ furniture-react.js/src/component/layout/Partner.jsx
@@ -4,17 +4,19 @@ import '../styles/Partner.css';
 const Partner = () => {
 
   // image streching, text color little hard to see.
-  // fix: change image || make container parralex and change text color.
+  // fix: change image || make container parralex and change text color.../// img src={require('../img/img-8.jpg')} alt=""
 
   return (
-    <section>
-        <img src={require('../img/partner-img.jpg')} alt="" className="partner-img" />
+
+    <section className="partner-section">
+        <div className="partner-img" ></div>
         <h5 className="partner-title">WE LOVE<br/>PARTNERSHIP</h5>
         <p className="partner-desc">
             Situation: your place, be it a city cafe,<br/> deli or bar, has got everything in the world. Almost.
             It is quite<br/> possible that you lack mr.pops. And this can be fixed.
         </p>
     </section>
+    
   )
 }
 
