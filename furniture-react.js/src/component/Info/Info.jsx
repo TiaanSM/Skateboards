169f4d61e429f56221a3d6c4bb9d3eba2806e283
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-11-25 15:47:36 +0200

    Added Info section basic layout.
    Info section basic structure and styling completed will improve when laptop ram upgraded.

--- furniture-react.js/src/component/Info/Info.jsx
+++ furniture-react.js/src/component/Info/Info.jsx
@@ -2,10 +2,26 @@
 
 const Info = () => {
   return (
-    <section>
-        <h2>WE HAVE SEVERAL CONDITIONS</h2>
-        <p></p>
-    </section>
+    <div className="container">
+      <div className="centerpiece"></div>
+      <p className="top-paragraph">
+        Do you want to become our partner? We are open to cooperation, but first we always get acquainted with the location to find "our own". So no offense -           there are also cases when we are destined to grow separately.
+      </p>
+      <div className="question-paragraph">
+        <h3 className="question-title">DEAL? THEN FILL OUT THE FORM</h3>
+        <p className="question-desc">
+          Just 10 basic questions that will take 10 minutes at most.
+          But these 10 minutes can be the start of our joint flight. It is not only for Musk and Bezos to conquer space.
+        </p>
+        <button className="question-btn">Fill out the questionnaire</button>
+      </div>
+      <div className="last-paragraph">
+        <h3 className="last-title">IT REMAINS TO WAIT FOR THE LETTER</h3>
+        <p className="last-desc">
+          After filling out the questionnaire, our manager will process it and send you detailed information about cooperation. This includes prices, equipment           rental conditions, and recommended retail price. After that we will be able to start. Even if the stars are not aligned and we are not on the way yet,           we will still write to you.
+        </p>
+      </div>
+     </div>
   )
 }
    
