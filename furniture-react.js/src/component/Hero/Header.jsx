14eee22286978129927e669ce2c4b228f6272ef4
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-11-27 19:20:18 +0200

    Worked on header.

--- furniture-react.js/src/component/Hero/Header.jsx
+++ furniture-react.js/src/component/Hero/Header.jsx
@@ -2,23 +2,35 @@ import React, { useState, useRef, useEffect } from 'react';
 import '../Hero/Header.css';
 import OverlayHero from './OverlayHero';
 
-import {motion, useScroll} from 'framer-motion';
-
 const Header = () => {
   
-  const { scrollY } = useScroll();
-    
-  useEffect(() => {
-    return scrollY.onChange((latest) => {
-      console.log("Page scroll:", latest / 100 * 1)
-    })
-  }, [])
+  const [count, setCount] = useState(0);
+  const [direction, setDirection] = useState('down');
 
 
+  const handleDirection = (event) => {
+
+  
+    const value = event.deltaY;
+
+    if (value > 0 && count <= 6) {
+      setCount((count) => count + 1);
+      setDirection((direction) => direction = 'down');
+
+    } else if (value < 0 && count >= 1) {
+      setCount((count) => count - 1);
+      setDirection((direction) => direction = 'up');
+    }
+  } 
+
+
+  console.log(count);
+  
   return (
-    <header>
+    <header onWheel={handleDirection} className={count >= 7 ? ('header-2') : ('header-1')}>
 
-      <div className="img-container" >
+      <div className="img-container" 
+      style={{ animation: `${direction}Scale${count} 0.2s ease forwards`}}>
         <img src={require('../img/header-img (3).jpg')} alt="Skater" />
       </div>
 
@@ -34,68 +46,7 @@ const Header = () => {
 export default Header
 
 /* 
-<header onWheel={handleDirection} ref={header}>
-
-      <div className="img-container" 
-      style={{ animation: `${direction}Scale${count} 0.2s ease forwards`}}>
-        <img src={require('../img/header-img (3).jpg')} alt="Skater" />
-      </div>
-      <OverlayHero />
-
-      <div className="background-text-container">
-        <span className="scrolling-texts">* WE ALWAYS CHECK THE QUALITY *<span className="hollow-texts"> WE ALWAYS CHECK THE QUALITY * </span>* ART ON MAPLE MATERPIECES *</span>
-        <span className="scrolling-texts"><span className="hollow-texts"> RECYCLED MATERIAL * </span>* RECYCLED MATERIALS *<span className="hollow-texts"> RECYCLED MATERIAL * </span></span>
-        <span className="scrolling-texts">* ART ON MAPLE MATERPIECES *<span className="hollow-texts"> ART ON MAPLE MASTERPIECES * </span>* ART ON MAPLE MATERPIECES *</span>
-      </div>
-    </header>
-
-  const [count, setCount] = useState(0);
-  const [direction, setDirection] = useState('down');
-  const [isVisible, setIsVisible] = useState(false);
-
-  const header = useRef(null);
-
-  const callbackFunction = (entries) => {
-    const [entry] = entries
-    setIsVisible(entry.isIntersecting)
-  }
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 1.0
-  }
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(callbackFunction, options)
-    if (header.current) observer.observe(header.current)
-
-    return () => {
-      if(header.current) observer.unobserve(header.current)
-    }
-  }, [header, options])
 
 
-  const handleDirection = (event) => {
-
-   if (isVisible) {
-    const value = event.deltaY;
-
-    if (value > 0 && count <= 6) {
-      setCount((count) => count + 1);
-      setDirection((direction) => direction = 'down');
-
-    } else if (value < 0 && count >= 1) {
-      setCount((count) => count - 1);
-      setDirection((direction) => direction = 'up');
-    }
-   } 
-  }
-
-  if (isVisible && count < 6) {
-    document.body.style.overflow = 'hidden'
-  } else if (isVisible && count === 7) {
-    document.body.style.overflow = ''
-  }
-
-  console.log(count);
+  
 */
--- furniture-react.js/src/component/Partner/Partner.jsx
+++ furniture-react.js/src/component/Partner/Partner.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import '../Partner/Partner.css';
 
+
 const Partner = () => {
 
   //  text color little hard to see.
   // fix: change image || make container parralex and change text color.../// img src={require('../img/img-8.jpg')} alt=""
 
   return (
-
+    
     <section className="partner-section">
-        <div className="partner-img" ></div>
+        <div className="partner-img" >
+        <img src={require("../img/img-8.jpg")} alt="" />
+        </div>
+        
+        
         <h5 className="partner-title">WE LOVE<br/>PARTNERSHIP</h5>
         <p className="partner-desc">
             Situation: your place, be it a city cafe,<br/> deli or bar, has got everything in the world. Almost.
