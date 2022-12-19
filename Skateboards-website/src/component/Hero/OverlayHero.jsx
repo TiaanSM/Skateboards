4ff5d671fec8682cd2839039667ad3d1ce85275a
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-12-19 16:44:48 +0200

    Converted css to modules.
    Converted hero overlay css to module.css and improved responsiveness of component. Also split the css into seperate file.

--- Skateboards-website/src/component/Hero/OverlayHero.jsx
+++ Skateboards-website/src/component/Hero/OverlayHero.jsx
@@ -1,5 +1,5 @@
 import IconButton from '../General/IconButton';
-import styles from './Header.module.css';
+import styles from './OverlayHero.module.css';
 
 const OverlayHero = () => {
 
@@ -13,7 +13,7 @@ const OverlayHero = () => {
         <p className={styles.overlayDesc}>
         No, girl, I don't like all these ice creams and frozen juices... When I was a kid I liked it, of course, but I don't know what else I liked... let's have the usual, white chocolate-covered... And what's that lilac soap you have? mr.pops? I'll take the bright yellow one, what's that? Mango-maracuya? Oh, it's cold! And what's that crunching on your teeth? Bones... cool! And can I bring this pink one, too!
         </p>
-        <IconButton text="FLAVOURS"/>
+        <IconButton text="SKATEBOARDS"/>
         <span className={styles.titleTail}>RDS</span>
       </div>
   </div>
