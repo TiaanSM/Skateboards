a2828669a8439570fea239c324b436574a81560a
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-12-21 19:07:27 +0200

    Added react slick for main carousel.
    Added slider packages and logic for basic example before restyling.

--- Skateboards-website/src/component/Main/Carousel.jsx
+++ Skateboards-website/src/component/Main/Carousel.jsx
@@ -1,17 +1,58 @@
-import React from 'react'
-import styles from './Carousel.module.css';
+import './Carousel.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 const Carousel = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 3000,
+    cssEase: "linear"
+  };
+
   return (
-    <section className={styles.carousel}>
-        <div className={styles.imageContainer}>
-            <img src={require('../img/img-7.jpg')} alt="" className={styles.carouselImage} />  
-            <img src={require('../img/img-4.jpg')} alt="" className={styles.carouselImage} /> 
-            <img src={require('../img/img-5.jpg')} alt="" className={styles.carouselImage} />
-            <img src={require('../img/img-9.jpg')} alt="" className={styles.carouselImage} />
+    <div className="container-slick">
+      <h2>Auto Play</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
         </div>
-    </section>
-  )
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
 }
 
 export default Carousel
+
+/*
+
+
+<img src={require('../img/img-7.jpg')} alt="" className={styles.carouselImage} />  
+           
+            <img src={require('../img/img-4.jpg')} alt="" className={styles.carouselImage} /> 
+         
+            <img src={require('../img/img-5.jpg')} alt="" className={styles.carouselImage} />
+         
+            <img src={require('../img/img-9.jpg')} alt="" className={styles.carouselImage} />
+*/
